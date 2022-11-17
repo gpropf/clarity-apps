5afdc7de908b808db507ca193483d7582f529ad5
Greg Propf <gpropf@gmail.com>
2022-11-16 18:07:40 -0800

    Beaker and BeakerNode class

--- Pixelreactor.hpp
+++ Pixelreactor.hpp
@@ -9,32 +9,53 @@
 
 using namespace clarity;
 
+template <typename V>
+class Beaker {
+   protected:
+    int gridWidth, gridHeight;
+    V *gridArray;
+};
+
+template <typename V>
+class BeakerNode : public HybridNode<V> {
+   public:
+    BeakerNode(const string &name, const string &tag, bool useExistingDOMElement,
+               ClarityNode::AttachmentMode attachmentMode = ClarityNode::AttachmentMode::NEW,
+               const string &attachmentId = "")
+        : HybridNode<V>(name, tag, useExistingDOMElement, attachmentMode, attachmentId) {}
+
+    inline virtual void doNothing() {
+        cout << "This method exists so that Embind will create a type when this method in bound."
+             << endl;
+    }
+};
+
+EMSCRIPTEN_BINDINGS(PixelReactor) {
+    class_<HybridNode<Beaker<unsigned char>>>("BeakerNode_h")
+        .function("doNothing", &HybridNode<Beaker<unsigned char>>::doNothing, allow_raw_pointers());
+}
+
 /**
  * @brief The Pixelreactor app from the old ClojureScript site redone in C++.
  *
  */
 struct PixelReactor : public PageContent {
-
-    
-    
-    
-    
     int *ruleFrameWidth = new int(5);
     int *ruleFrameHeight = new int(3);
-
 
     ClarityNode *content(ClarityNode *innerContent = nullptr) {
         ClarityNode::addJSAuxScript("clapps-aux.js");
         ClarityNode::runJSAuxScripts();
-        
+
         val CLElement = val::global("CLElement");
-        val blackbody_st = ClarityNode::JSProxyNode_["blackbody_st"];
+        // val blackbody_st = ClarityNode::JSProxyNode_["blackbody_st"];
         CLNodeFactory<HybridNode, double, double> builder("div", "maindiv");
         auto *maindiv = builder.build();
 
         CLNodeFactory<HybridNode, string, double> textBuilder(builder.withChildrenOf(maindiv));
         CLNodeFactory<HybridNode, int, int> intBuilder(builder.withChildrenOf(maindiv));
-        //builder = builder->
+
+        // builder = builder->
 
         CLNodeFactory<HybridNode, unsigned char, double> canvasBuilder(builder);
 
@@ -58,21 +79,28 @@ struct PixelReactor : public PageContent {
         maindiv->appendChild(canvas1CurrentCellColor_tinp);
         builder.br();
 
-        //CLNodeFactory<HybridNode, string, double> textBuilder(builder.withChildrenOf(maindiv));
+        // CLNodeFactory<HybridNode, string, double> textBuilder(builder.withChildrenOf(maindiv));
         string *cmdarea_text = new string("This is a textarea.");
         auto *cmdarea = textBuilder.withName("cmdarea").textarea(cmdarea_text, 6, 80);
         textBuilder.br();
         auto *cmdarea_lbl = textBuilder.label(cmdarea, "CMD:", true);
         textBuilder.br();
 
-        auto * ruleFrameWidth_tinp = intBuilder.withName("ruleFrameWidth_tinp").withCppVal(ruleFrameWidth).textInput();
-        auto * ruleFrameWidth_tinp_lbl = intBuilder.label(ruleFrameWidth_tinp, "Width of new rule frames.", true);
-        auto * ruleFrameHeight_tinp = intBuilder.withName("ruleFrameHeight_tinp").withCppVal(ruleFrameHeight).textInput();
-        auto * ruleFrameHeight_tinp_lbl = intBuilder.label(ruleFrameHeight_tinp, "Height of new rule frames.", true);
-
-
+        auto *ruleFrameWidth_tinp =
+            intBuilder.withName("ruleFrameWidth_tinp").withCppVal(ruleFrameWidth).textInput();
+        auto *ruleFrameWidth_tinp_lbl =
+            intBuilder.label(ruleFrameWidth_tinp, "Width of new rule frames.", true);
+        auto *ruleFrameHeight_tinp =
+            intBuilder.withName("ruleFrameHeight_tinp").withCppVal(ruleFrameHeight).textInput();
+        auto *ruleFrameHeight_tinp_lbl =
+            intBuilder.label(ruleFrameHeight_tinp, "Height of new rule frames.", true);
 
         textBuilder.br();
+
+        CLNodeFactory<BeakerNode, Beaker<unsigned char>, int> beakerBuilder(
+            builder.withChildrenOf(maindiv));
+        auto *b = new Beaker<unsigned char>();
+        auto *bn = beakerBuilder.withTag("div").withName("mainBeaker").withCppVal(b).build();
 
         printf("Setup complete!\n");
         return maindiv;
